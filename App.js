e73c84bcfd13a6385271327b694a3beec1228aad
Luca Ford <lucaford@Lucas-MacBook-Pro.local>
2020-02-16 12:49:38 -0300

    add city input

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, AsyncStorage} from 'react-native';
+import {ifIphoneX} from 'react-native-iphone-x-helper';
 
 import Map from './src/components/Maps/Map';
+import CityInput from './src/components/CityInput/CityInput';
+import WeatherResponseExample from './src/utils/WeatherResponseExample';
 
-const App = () => (
-  <View style={styles.container}>
-    <View style={styles.mapContainer}>
-      <Map />
+const App = () => {
+  const [cityInput, setCityInput] = useState('');
+  const [lastSearches, setLastSearches] = useState([]);
+
+  const handleChangeCityInputText = value => {
+    setCityInput(value);
+  };
+
+  const handleSearchByCityPress = async () => {
+    setLastSearches(cities => cities.concat(cityInput));
+    await AsyncStorage.setItem('lastSearches', JSON.stringify(lastSearches));
+    setCityInput('');
+
+    console.log(WeatherResponseExample.coord);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cityInputContainer}>
+        <CityInput
+          onChangeCityInputText={handleChangeCityInputText}
+          onSearchByCityPress={handleSearchByCityPress}
+          value={cityInput}
+        />
+      </View>
+      <View style={styles.mapContainer}>
+        <Map />
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  cityInputContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    ...ifIphoneX(
+      {
+        paddingTop: 50,
+      },
+      {
+        paddingTop: 20,
+      },
+    ),
   },
 
   mapContainer: {
